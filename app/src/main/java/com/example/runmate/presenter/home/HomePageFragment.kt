31859ddfc0c8b7fbf76cfb9ce603f5a023b344a3
Egorov Mikhail <m-5321e@yandex.com>
2024-01-07 16:23:51 +0500

    Added base xml layout

--- app/src/main/java/com/example/runmate/presenter/home/HomePageFragment.kt
+++ app/src/main/java/com/example/runmate/presenter/home/HomePageFragment.kt
@@ -5,12 +5,12 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.runmate.R
-import com.example.runmate.databinding.FragmentHomeBinding
+import com.example.runmate.databinding.FragmentHomePageBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home) {
-    private val binding: FragmentHomeBinding by viewBinding()
+class HomePageFragment : Fragment(R.layout.fragment_home_page) {
+    private val binding: FragmentHomePageBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
